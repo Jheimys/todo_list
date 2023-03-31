d6cf8224885f7bba21733099bb5e7785764ad22c
Jheimys Bassani <jheimysbb@gmail.com>
2023-03-31 18:25:52 -0300

    :construction: Criando as funções e evento do js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,8 @@
     const editForm = document.getElementById('edit-form')
     const editInput = document.getElementById('edit-input')
     const cancelEditBtn = document.getElementById('cancel-edit-btn')
+
+    let oldInputValue
    
     //Funções
     const saveTodo = (text) => {
@@ -40,6 +42,24 @@
         todoInput.focus()
     }
 
+    const toggleForms = () => {
+        editForm.classList.toggle('hide')
+        todoForm.classList.toggle('hide')
+        todoList.classList.toggle('hide')
+    }
+
+    const updateTodo = (text) => {
+        const todos = document.querySelectorAll('.todo')
+
+        todos.forEach((todo) => {
+            let todoTitle = todo.querySelector('h3')
+
+            if(todoTitle.innerText === oldInputValue) {
+                todoTitle.innerText = text
+            }
+        })
+    }
+
     //Eventos
     todoForm.addEventListener('submit', (e) => {
         e.preventDefault()
@@ -50,4 +70,49 @@
             saveTodo(inputValue)
         }
     })
+
+    document.addEventListener('click', (e) => {
+        const targetEl = e.target
+        const parentEl = targetEl.parentNode
+        let todoTitle
+
+        if(parentEl && parentEl.querySelector('h3')) {
+            todoTitle = parentEl.querySelector('h3').innerText
+        }
+
+        if(targetEl.classList.contains('finish-todo')){
+            parentEl.classList.toggle('done')
+        }
+
+        if(targetEl.classList.contains('remove-todo')){
+            parentEl.remove()
+        }
+
+        if(targetEl.classList.contains('edit-todo')){
+            toggleForms()
+
+            editInput.value = todoTitle
+            oldInputValue = todoTitle
+        }
+    })
+
+    cancelEditBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+
+        toggleForms()
+    })
+
+    editForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        const editInputValue = editInput.value
+
+        if(editInputValue ) {
+            //Atualizar
+            updateTodo(editInputValue)
+        }
+
+        toggleForms()
+    })
+
 })();
